12782e7e8348e08f0c3444c541f690a25e0226c1
wdxmlhf <151189232+wdxmlhf@users.noreply.github.com>
2025-11-19 16:14:03 +0800

    Updated index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -888,7 +888,7 @@ function Home() {
                   </button>
                 </div>
               ) : (
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+                <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
                   {hotAgents.map((agent) => (
                     <AgentCard
                       key={agent.agentCode}
